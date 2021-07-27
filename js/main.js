6e5fb6c8ac3feb28dc8f96626c1d4e6e2982bb8c
kamol0007 <59263172+kamol0007@users.noreply.github.com>
2021-07-27 17:26:37 +0500

    initial

---
+++ js/main.js
@@ -0,0 +1,31 @@
+/*Nav Mobile*/
+document.getElementById("open-btn").onclick = function() {
+    document.getElementById("mobile_nav").style.display='block';
+    document.body.style.overflow = 'hidden';
+}
+document.getElementById("close-btn").onclick = function() {navClose();}
+function navClose() {
+    document.getElementById("mobile_nav").style.display='none';
+    document.body.style.overflow = null;
+}
+
+/*Scroll*/ 
+window.onscroll = function() {Scrolling();};
+var header = document.getElementById("header");
+var back_to_top = document.getElementById("back-to-top");
+var sticky = false;
+function Scrolling() {
+    if(window.pageYOffset>50){
+        if(!sticky){
+            header.classList.add("scroll-header"); 
+            back_to_top.style.display='inline'; 
+        } 
+        sticky=true;
+    }else{
+        if(sticky){
+            header.classList.remove("scroll-header"); 
+            back_to_top.style.display='none'; 
+        } 
+        sticky=false;
+    }
+}
