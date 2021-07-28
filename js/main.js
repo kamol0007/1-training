2f6a983928c9cd27d45c11d4296636319ade5ae5
kamol0007 <59263172+kamol0007@users.noreply.github.com>
2021-07-28 17:49:48 +0500

    updated

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 /*Nav Mobile*/
+var mobile_nav = document.getElementById("mobile_nav");
 document.getElementById("open-btn").onclick = function() {
-    document.getElementById("mobile_nav").style.display='block';
+    mobile_nav.style.display='block';
     document.body.style.overflow = 'hidden';
-}
-document.getElementById("close-btn").onclick = function() {navClose();}
-function navClose() {
-    document.getElementById("mobile_nav").style.display='none';
+};
+document.getElementById("close-btn").onclick =  function navClose() {
+    mobile_nav.style.display='none';
     document.body.style.overflow = null;
-}
+};
 
 /*Scroll*/ 
 window.onscroll = function() {Scrolling();};
-var header = document.getElementById("header");
-var back_to_top = document.getElementById("back-to-top");
-var sticky = false;
+var header = document.getElementById("header"), 
+    back_to_top = document.getElementById("back-to-top"),
+    sticky = false;
 function Scrolling() {
     if(window.pageYOffset>50){
         if(!sticky){
@@ -30,10 +30,8 @@ function Scrolling() {
     }
 }
 
-
 /*Add post*/
-
-const form = document.getElementById("add-post-form"), 
+var form = document.getElementById("add-post-form"), 
     imgInp = document.getElementById("upload"),
     addPost = document.getElementById("add-post"),
     addPostItem = document.getElementById("add-post-item"),
@@ -42,37 +40,39 @@ const form = document.getElementById("add-post-form"),
     postDiscription = document.getElementById("post_discription"),
     uploadImg = document.getElementById("upload_img"),
     uploadLabel = document.getElementById("upload_label"),
-    postTarget = document.getElementById("post_target");
-
-
-
+    postTarget = document.getElementById("post_target"),
+    post_last_id=1;
+    
 addPost.onclick = function() {
     this.style.display='none';
     addPostItem.style.display='block';
-}
+    addPostItem.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+};
 postSaveClose.onclick = function() {
     addPostItem.style.display='none';
     addPost.style.display='block';
     postTitle.style.borderColor='#ced4da';
     postDiscription.style.borderColor='#ced4da';
+    uploadLabel.style.borderColor='#ced4da';
     postTitle.value = '';
     postDiscription.value = '';
     uploadImg.src = 'img/upload.png';
     imgInp.value = null;
-}
+    if(post_last_id>1) document.getElementById("post-"+(post_last_id-1) ).scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+    else addPost.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+};
 postTitle.onchange = function() {
     this.style.borderColor='#ced4da';
-}
+};
 postDiscription.onchange = function() {
     this.style.borderColor='#ced4da';
-}
-
+};
 imgInp.onchange = function(evt){
     uploadLabel.style.borderColor='#ced4da';
     const [file] = evt.target.files;
     if (file) uploadImg.src = URL.createObjectURL(file);
-}
-var add_post = function(event) {
+};
+var add_post = function(event){
     event.preventDefault();
     const title = event.target[0].value, img = event.target[1], discription = event.target[2].value;
     const [file] = img.files;
@@ -92,7 +92,7 @@ var add_post = function(event) {
     }
     if(title_check&&discription_check&&img_check){
         postTarget.insertAdjacentHTML('beforeend', 
-            '<div class="post-item"><div class="img-target"><img src="'+src+'" alt="img" class="img-fluid"></div><div class="context"><div><span>t</span><span>b</span></div><h4>'+title+'</h4><p>'+discription+'</p></div></div>'
+            '<div id="post-'+post_last_id+'" class="post-item"><div class="img-target"><img src="'+src+'" alt="img" class="img-fluid"></div><div class="context"><div><span>t</span><span>b</span></div><h4>'+title+'</h4><p>'+discription+'</p></div></div>'
         );
         addPostItem.style.display='none';
         addPost.style.display='block';
@@ -100,6 +100,8 @@ var add_post = function(event) {
         postDiscription.value = '';
         uploadImg.src = 'img/upload.png';
         imgInp.value = null;
+        document.getElementById("post-"+post_last_id).scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+        post_last_id++;
     }
 };
 form.addEventListener("submit", add_post, true);
